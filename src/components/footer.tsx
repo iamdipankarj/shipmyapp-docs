5fa874fc7cb45353bed74fecdfb9fe9f8002bbbb
iamdipankarj <iamdipankarj@gmail.com>
2024-04-09 12:50:56 +0530

    update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,17 +4,17 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="bg-base-200 border-t border-base-content/10">
-      <div className="max-w-7xl mx-auto px-8 py-24">
-        <div className=" flex lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
-          <div className="w-80 max-w-full flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
+    <footer className="bg-base-200">
+      <div className="container">
+        <div className="grid xl:grid-cols-5 gap-6 py-16">
+          <div className="xl:col-span-2">
             <Logo className="w-32" />
-            <p className="mt-3 text-sm text-base-content/80 leading-relaxed">
-              A No-Nonsense NextJS Starter kit for Startups.
+            <p className="mt-3 text-md text-base-content/80 leading-relaxed">
+              A No-Nonsense NextJS Boilerplate for Startups.
               <br />
-              Made with ❤️ by <a href="https://twitter.com/iamdipankarj" className="link link-success" target="_blank" rel="nofollow">@iamdipankarj</a>
+              Made with ❤️ by <a href="https://twitter.com/iamdipankarj" className="link link-primary" target="_blank" rel="nofollow">@iamdipankarj</a>
             </p>
-            <a
+            <Link
               className="inline-block mt-4 text-sm border border-base-content/20 hover:border-base-content/40 hover:text-base-content/90 hover:bg-base-300 duration-200 cursor-pointer rounded text-base-content/80 px-2 py-1"
               href="/"
             >
@@ -29,57 +29,46 @@ export function Footer() {
                 />
                 <span className="font-bold text-base-content flex gap-0.5 items-center tracking-tight">ShipMyApp</span>
               </div>
-            </a>
+            </Link>
           </div>
-          <div className="flex-grow flex flex-wrap md:pl-24 -mb-10 md:mt-0 mt-10 text-center md:text-left">
-            <div className="lg:w-1/3 md:w-1/2 w-full px-4">
-              <div className="footer-title font-semibold text-base-content tracking-widest text-sm md:text-left mb-3">
-                LINKS
+          <div className="xl:col-span-3 col-span-4">
+            <div className="flex flex-col sm:flex-row gap-6 flex-wrap justify-between">
+              <div>
+                <div className="flex flex-col gap-3">
+                  <h5 className="mb-3 font-semibold uppercase">LINKS</h5>
+                  <Link className="link link-hover text-gray-500/80" href="/#pricing">
+                    Pricing
+                  </Link>
+                  <Link className="link link-hover text-gray-500/80" target="_blank" href="https://docs.shipmyapp.com/">
+                    Documentation
+                  </Link>
+                  <Link
+                    href="mailto:iamdipankarj.workspace@gmail.com"
+                    target="_blank"
+                    className="link link-hover text-gray-500/80"
+                  >
+                    Support
+                  </Link>
+                </div>
               </div>
-              <div className="flex flex-col justify-center items-center md:items-start gap-2 mb-10 text-sm">
-                <a className="link link-hover" href="/#pricing">
-                  Pricing
-                </a>
-                <a className="link link-hover" href="/license">
-                  Licenses
-                </a>
-                <a className="link link-hover" href="/docs">
-                  Documentation
-                </a>
-                <a
-                  href="mailto:marc@shipfa.st"
-                  target="_blank"
-                  className="link link-hover"
-                >
-                  Support
-                </a>
+              <div>
+                <div className="flex flex-col gap-3">
+                  <h5 className="mb-3 uppercase">LEGAL</h5>
+                  <Link className="link link-hover text-gray-500/80" href="/tos">
+                    Terms of Service
+                  </Link>
+                  <Link className="link link-hover text-gray-500/80" href="/privacy-policy">
+                    Privacy Policy
+                  </Link>
+                </div>
               </div>
-            </div>
-            <div className="lg:w-1/3 md:w-1/2 w-full px-4">
-              <div className="footer-title font-semibold text-base-content tracking-widest text-sm md:text-left mb-3">
-                LEGAL
-              </div>
-              <div className="flex flex-col justify-center items-center md:items-start gap-2 mb-10 text-sm">
-                <Link className="link link-hover" href="/tos">
-                  Terms of Service
-                </Link>
-                <Link className="link link-hover" href="/privacy-policy">
-                  Privacy Policy
-                </Link>
-              </div>
-            </div>
-            <div className="lg:w-1/3 md:w-1/2 w-full px-4">
-              <div className="footer-title font-semibold text-base-content tracking-widest text-sm md:text-left mb-3">
-                Social
-              </div>
-              <div className="flex flex-col justify-center items-center md:items-start gap-2 mb-10 text-sm">
-                <a
-                  href="https://twitter.com/iamdipankarj"
-                  target="_blank"
-                  className="link link-hover"
-                >
-                  @iamdipankarj
-                </a>
+              <div>
+                <div className="flex flex-col gap-3">
+                  <h5 className="mb-3 uppercase">Social Media</h5>
+                  <div className="text-gray-500/80">
+                    <Link className="link link-hover" href="https://twitter.com/iamdipankarj">X: @iamdipankarj</Link>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
